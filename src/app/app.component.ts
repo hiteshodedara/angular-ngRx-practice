d003f3ec9413032c759cfffc77626d697ef30bcd
Odedara Hitesh <hiteshodedara1010@gmail.com>
2024-01-05 15:55:21 +0530

    ok now trying ngrx store

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BoardsService } from './services/boards.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ngRx-practice';
+
+  boards$:any;
+
+  constructor(private boardsService: BoardsService){
+  }
+ 
+
+
+
+  get_boards(){
+   
+
+
+    this.boardsService.getWorkspaceById(1).subscribe(res=>{
+      console.log("workspaces:",res);
+      
+    })
+    this.boardsService.getAllBoards(1).subscribe(res=>{
+      console.log("boards:",res);
+      
+    })
+    this.boardsService.getAllTodoLists(1,1).subscribe(res=>{
+      console.log("todolist:",res);
+    })
+    this.boardsService.getAllTodos(1,1).subscribe(res=>{
+      console.log("todos:",res);
+      
+    })
+
+  }
+
 }
